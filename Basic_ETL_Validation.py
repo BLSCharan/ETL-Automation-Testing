0f357bf1f67b97b160668c81b5f5c7d12207279b
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2026-02-27 16:50:16 +0530

    Update Basic_ETL_Validation.py

--- Basic_ETL_Validation.py
+++ Basic_ETL_Validation.py
@@ -37,9 +37,9 @@ except Exception as e:
 
 try:
     sf_connection = snowflake.connector.connect(
-        user='BLSCHARAN',
-        password='Scheryy@12345678',
-        account='myhrjeq-zq60870',
+        user='*****',
+        password='*******',
+        account='*******',
         warehouse='ETL_WH',
         database='ETL_DB',
         schema='PUBLIC',
